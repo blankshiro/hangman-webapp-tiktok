0d3254542a7af4235d7d879a703f05c2a2dddd0c
gjw00 <jiawey03@gmail.com>
2022-05-27 22:33:09 +0900

    mobile

--- src/App.js
+++ src/App.js
@@ -6,72 +6,94 @@ import Word from "./components/Word";
 import randomWords from "random-words";
 import Popup from "./components/Popup";
 import Notification from "./components/Notification";
+import Keyboard from "./components/Keyboard";
+import { isMobile } from "react-device-detect";
+
 import { showNotification as notify } from "./helpers/notification";
 import "./App.css";
+
 let generatedWord = randomWords();
 console.log(generatedWord);
 
 function App() {
-    const [playable, setPlayable] = useState(true);
-    const [correctLetters, setCorrectLetters] = useState([]);
-    const [wrongLetters, setWrongLetters] = useState([]);
-    const [showNotification, setShowNotification] = useState(false);
+  const [playable, setPlayable] = useState(true);
+  const [correctLetters, setCorrectLetters] = useState([]);
+  const [wrongLetters, setWrongLetters] = useState([]);
+  const [showNotification, setShowNotification] = useState(false);
 
-    useEffect(() => {
-        const handleKeydown = (event) => {
-            const { key, keyCode } = event;
-            // if playable state and key is a letter
-            if (playable && keyCode >= 65 && keyCode <= 90) {
-                const letter = key.toLowerCase();
-                // check if current letter is in generated word
-                if (generatedWord.includes(letter)) {
-                    // if yes, add letter to correctLetters or check if game is won
-                    if (!correctLetters.includes(letter)) {
-                        // add current letter to correctLetters
-                        setCorrectLetters((correctLetters) => [...correctLetters, letter]);
-                    } else {
-                        notify(setShowNotification);
-                    }
-                    // if no, add letter to wrongLetters or check if game is lost
-                } else {
-                    if (!wrongLetters.includes(letter) & (wrongLetters.length < 6)) {
-                        // add current letter to wrongLetters
-                        setWrongLetters((wrongLetters) => [...wrongLetters, letter]);
-                    } else {
-                        notify(setShowNotification);
-                    }
-                }
-            }
-        };
-
-        // listen for keypress and pass it to handleKeydown function
-        window.addEventListener("keydown", handleKeydown);
-        // return function to remove event listener
-        return () => window.removeEventListener("keydown", handleKeydown);
-    }, [correctLetters, wrongLetters, playable]); // dependencies
-
-    function playAgain() {
-        // set playable state to true
-        setPlayable(true);
-        // reset correctLetters array
-        setCorrectLetters([]);
-        // reset wrongLetters array
-        setWrongLetters([]);
-        // generate new word
-        generatedWord = randomWords();
+  const validateKey = (key) => {
+    const letter = key.toLowerCase();
+    if (generatedWord.includes(letter)) {
+      // if yes, add letter to correctLetters or check if game is won
+      if (!correctLetters.includes(letter)) {
+        // add current letter to correctLetters
+        setCorrectLetters((correctLetters) => [...correctLetters, letter]);
+      } else {
+        notify(setShowNotification);
+      }
+      // if no, add letter to wrongLetters or check if game is lost
+    } else {
+      if (!wrongLetters.includes(letter) & (wrongLetters.length < 6)) {
+        // add current letter to wrongLetters
+        setWrongLetters((wrongLetters) => [...wrongLetters, letter]);
+      } else {
+        notify(setShowNotification);
+      }
     }
+  };
 
-    return (
-        <div>
-            <Header />
-            <div>
-                <Figure wrongLetters={wrongLetters} />
-                <Mistakes wrongLetters={wrongLetters} />
-                <Word generatedWord={generatedWord} correctLetters={correctLetters} />
-            </div>
-            <Popup correctLetters={correctLetters} wrongLetters={wrongLetters} generatedWord={generatedWord} setPlayable={setPlayable} playAgain={playAgain} />
-            <Notification showNotification={showNotification} />
-        </div>
-    );
+  useEffect(() => {
+    console.log(isMobile);
+    const handleKeydown = (event) => {
+      const { key, keyCode } = event;
+      // if playable state and key is a letter
+      if (playable && keyCode >= 65 && keyCode <= 90) {
+        validateKey(key);
+      }
+    };
+
+    // listen for keypress and pass it to handleKeydown function
+
+    if (!isMobile) {
+      window.addEventListener("keydown", handleKeydown);
+
+      // return function to remove event listener
+      return () => window.removeEventListener("keydown", handleKeydown);
+    } else {
+    }
+  }, [correctLetters, wrongLetters, playable]); // dependencies
+
+  function playAgain() {
+    // set playable state to true
+    setPlayable(true);
+    // reset correctLetters array
+    setCorrectLetters([]);
+    // reset wrongLetters array
+    setWrongLetters([]);
+    // generate new word
+    generatedWord = randomWords();
+  }
+
+  return (
+    <div>
+      <Header />
+      <div>
+        <Figure wrongLetters={wrongLetters} />
+        <Mistakes wrongLetters={wrongLetters} />
+        <Word generatedWord={generatedWord} correctLetters={correctLetters} />
+      </div>
+      <Popup
+        correctLetters={correctLetters}
+        wrongLetters={wrongLetters}
+        generatedWord={generatedWord}
+        setPlayable={setPlayable}
+        playAgain={playAgain}
+      />
+      <Notification showNotification={showNotification} />
+
+      {isMobile && <br />}
+      {isMobile && <Keyboard keyPress={validateKey} />}
+    </div>
+  );
 }
 export default App;
---
+++ src/components/Keyboard.js
@@ -0,0 +1,112 @@
+import * as React from "react";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Stack";
+import "../App.css";
+
+export default function Keyboard(props) {
+  /* if not using qwerty format
+  const alphaLength = Array.from(Array(26).keys());
+  const alphabet = alphaLength.map((x) => String.fromCharCode(x + 65));
+
+  alphabet.map((letter, index) => {
+    index === 0 && console.log(letter);
+  });
+  */
+  const alphabet = [
+    "Q",
+    "W",
+    "E",
+    "R",
+    "T",
+    "Y",
+    "U",
+    "I",
+    "O",
+    "P",
+    "A",
+    "S",
+    "D",
+    "F",
+    "G",
+    "H",
+    "J",
+    "K",
+    "L",
+    "Z",
+    "X",
+    "C",
+    "V",
+    "B",
+    "N",
+    "M",
+  ];
+
+  return (
+    <div>
+      <Stack
+        spacing={1.5}
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+      >
+        {alphabet.map((letter, index) => {
+          return (
+            index < 10 && (
+              <button
+                className="button"
+                key={letter}
+                onClick={() => props.keyPress(letter)}
+              >
+                {letter}
+              </button>
+            )
+          );
+        })}
+      </Stack>
+      <br />
+      <Stack
+        spacing={1.5}
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+      >
+        {alphabet.map((letter, index) => {
+          return (
+            index >= 10 &&
+            index < 19 && (
+              <button
+                className="button"
+                key={letter}
+                onClick={() => props.keyPress(letter)}
+              >
+                {letter}
+              </button>
+            )
+          );
+        })}
+      </Stack>
+      <br />
+      <Stack
+        spacing={1.5}
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+      >
+        {alphabet.map((letter, index) => {
+          return (
+            index >= 19 && (
+              <button
+                className="button"
+                key={letter}
+                onClick={() => props.keyPress(letter)}
+              >
+                {letter}
+              </button>
+            )
+          );
+        })}
+      </Stack>
+      <br />
+    </div>
+  );
+}
